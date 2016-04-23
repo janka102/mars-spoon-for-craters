94229631900d58227296e0fbfd6cc5da04df7ed0
Jesse Smick <janka102@gmail.com>
2016-04-23 15:38:01 -0700

    Init data

---
+++ com.github.janka102.minecraft/src/com/github/janka102/minecraft/MarsSpoon.java
@@ -0,0 +1,13 @@
+package com.github.janka102.minecraft;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MarsSpoon extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    }
+}
