b1c8b18eebee3302941596c49e89095c4b4bb057
Jesse Smick <janka102@gmail.com>
2016-04-24 11:15:53 -0700

    Loads ALL THE DATA

--- com.github.janka102.minecraft/src/com/github/janka102/minecraft/MarsChunkGenerator.java
+++ com.github.janka102.minecraft/src/com/github/janka102/minecraft/MarsChunkGenerator.java
@@ -1,5 +1,6 @@
 package com.github.janka102.minecraft;
 
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import org.bukkit.Material;
@@ -11,14 +12,16 @@ public class MarsChunkGenerator extends ChunkGenerator {
 	public ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, ChunkGenerator.BiomeGrid biome) {
 		ChunkGenerator.ChunkData chunkData = createChunkData(world);
 		
-		int imgX = (Math.abs(x) / 256) % 64;
-		int imgY = (Math.abs(y) / 256) % 31;
+		int imgX = (Math.abs(x) / 16) % 64;
+		int imgY = (Math.abs(z) / 16) % 31;
+
 		if (MarsSpoon.tiles[imgY][imgX] == null) {
 			MarsSpoon.loadTile(imgY, imgX);
 		}
 		// code needs to be modified to use imgX and imgY.
 		// negative values should "wrap around" to the opposite side of the image
 		
+		BufferedImage tile = MarsSpoon.tiles[imgY][imgX];
 
 		int offsetX = Math.abs(x * 16) % 256;
 		int offsetZ = Math.abs(z * 16) % 256;
@@ -42,7 +45,7 @@ public class MarsChunkGenerator extends ChunkGenerator {
 
 		for (int i = 0; i < 16; i++) {
 			for (int j = 0; j < 16; j++) {
-				height = MarsSpoon.img.getRaster().getPixel(offsetX + (xExtra + i*xAdd), offsetZ + zExtra + j*zAdd, new int[4])[0];
+				height = tile.getRaster().getPixel(offsetX + (xExtra + i*xAdd), offsetZ + zExtra + j*zAdd, new int[4])[0];
 				chunkData.setRegion(i, 0, j, i + 1, height, j + 1, Material.STONE);
 			}
 		}
--- com.github.janka102.minecraft/src/com/github/janka102/minecraft/MarsSpoon.java
+++ com.github.janka102.minecraft/src/com/github/janka102/minecraft/MarsSpoon.java
@@ -1,6 +1,5 @@
 package com.github.janka102.minecraft;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -9,25 +8,15 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MarsSpoon extends JavaPlugin {
-	
-	static BufferedImage tiles = BufferedImage[32][64];
+	static BufferedImage[][] tiles = new BufferedImage[32][64];
 	
 	public static void loadTile(int imgY, int imgX) {
 		try {
-			tiles[imgY][imgX] = ImageIO.read(new File("images/" + imgY + "-" + imgX + ".png"));
+			tiles[imgY][imgX] = ImageIO.read(MarsSpoon.class.getResource("/" + imgY + "-" + imgX + ".png"));
 		} catch (IOException e) {
 			System.out.println("error" + " " + imgY + "-" + imgX + ".png ");
 		}
 	}
-
-    // Fired when plugin is first enabled
-    @Override
-    public void onEnable() {
-    }
-    // Fired when plugin is disabled
-    @Override
-    public void onDisable() {
-    }
     
     @Override
     public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
